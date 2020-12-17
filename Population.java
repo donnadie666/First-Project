c9ef45c41a69c66190b5c5a3ee8d71b0762deb4c
donnadie666 <76177632+donnadie666@users.noreply.github.com>
2020-12-17 12:05:46 -0600

    Add files via upload

---
+++ Population.java
@@ -0,0 +1,12 @@
+<<<<<<< HEAD
+import java.util.Arrays;
+import java.util.Coparator;
+import java.util.Random;
+public class Population{
+    private Individual population[];
+    private double populationFitness=-1;
+=======
+public class Population{
+	
+>>>>>>> 89d6facfc91a35a3647ffc86b9060766bbac5189
+}
